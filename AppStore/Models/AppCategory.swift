88a5a10f41e3d39431914acc1cd193e4cea31aaf
lpuentes19 <luey.puentes@gmail.com>
2017-12-04 16:46:00 -0700

    Created object models

---
+++ AppStore/Models/AppCategory.swift
@@ -0,0 +1,15 @@
+//
+//  AppCategory.swift
+//  AppStore
+//
+//  Created by Luis Puentes on 12/4/17.
+//  Copyright © 2017 LuisPuentes. All rights reserved.
+//
+
+import Foundation
+
+class AppCategory {
+    
+    var name: String?
+    var app: [App]?
+}
---
+++ AppStore/Models/Apps.swift
@@ -0,0 +1,18 @@
+//
+//  Apps.swift
+//  AppStore
+//
+//  Created by Luis Puentes on 12/4/17.
+//  Copyright © 2017 LuisPuentes. All rights reserved.
+//
+
+import Foundation
+
+class App {
+    
+    var id: Int?
+    var title: String?
+    var category: String?
+    var price: Int?
+    var appImage: String?
+}
